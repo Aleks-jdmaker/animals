dbd5b0dbfacfaa941a4eda87d19a296116f1d531
Alexander <jdmaker@gmail.com>
2022-12-19 20:55:02 +0300

    добавлены комменатрии в Main

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,21 +1,25 @@
 import animals.*;
 
-import java.util.ArrayList;
-import java.util.List;
+import java.util.List; // Список объектов. Это интерфейс (абстрактный тип) им нельзя пользоваться напрямую.
+import java.util.ArrayList; // Реализация списка. Можно создавать экземпляры (инстансы).
 
 public class Main {
-
-    private static final List<Predator> predatorCage = new ArrayList();
-    private static final List<Herbivore> herbivoreCage = new ArrayList();
-    private static final List<Animal> animals = new ArrayList();
+        private static final int ANIMAL_COUNT = 10;
+    private static final List<Predator> predatorCage = new ArrayList(); // Клетка с хищниками.
+    // <Predator> - дженерик (generic), допустимый тип для данного листа.
+    private static final List<Herbivore> herbivoreCage = new ArrayList(); // Клетка с травоядными.
+    private static final List<Animal> animals = new ArrayList(); // Исходный список животных
+    // (как хищников, так и травоядных).
     public static void main(String[] args) {
-        for (int i = 0; i < 10; i++) animals.add(Animal.createRandomAnimal());
-        // добавить животных и принудительно посадить хищника в клетку травоядных.
-        // try catch
-        for (Animal animal : animals) {
-            if (animal instanceof Predator) predatorCage.add((Predator) animal); // Преобразование типов (cast).
+        for (int i = 0; i < ANIMAL_COUNT; i++) animals.add(Animal.createRandomAnimal()); // Создаём несколько животных
+        // со случайным типом.
+        for (Animal animal : animals) { // Итерация (обход списка). Loop = цикл.
+            if (animal instanceof Predator) predatorCage.add((Predator) animal); // Преобразование типов (cast) - (Predator).
+            // Проверка, что текущее живтоное хищник.
+            // При добавении в список животное принудительно превращается в хищника. См. строку 8.
             else herbivoreCage.add((Herbivore) animal);
         }
+        // Выводим списки животных в клетках.
         System.out.println("Predators: ");
         for (Animal animal : predatorCage) System.out.println(" * " + animal);
         System.out.println("Herbivores: ");
